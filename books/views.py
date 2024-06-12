fc56e9eb0c21f348975f92fded9bb5df9f16869f
Vladyslav Shepilov <shepsterya@gmail.com>
2024-06-12 12:19:48 +0300

    add api/books endpoint

--- books/views.py
+++ books/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from books.serializers import (
+    BookSerializer,
+    BookListSerializer
+)
+
+from books.models import Book
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return BookListSerializer
+
+        return BookSerializer
